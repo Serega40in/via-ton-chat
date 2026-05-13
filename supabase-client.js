22d827908d538bffc53cdc2c1d355b8b25942fbf
Serega40in <serega40-in@yandex.ru>
2026-05-13 16:48:39 +0300

    Update Supabase anon key in supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // supabase-client.js
 const SUPABASE_URL = 'https://vwarulougzcsohoybxmv.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_McRGklsHAddHiLKspw3G9w_JIfRKnX_';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3YXJ1bG91Z3pjc29ob3lieG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjY0MzMsImV4cCI6MjA5NDI0MjQzM30.tP-qdcfAruG2zA41FXkqM8fRGCi23Yjm8neQNidB2s0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
